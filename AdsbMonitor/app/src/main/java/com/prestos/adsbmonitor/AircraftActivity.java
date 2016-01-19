b2b6757066e3ba117c3e1d256a8db6a448ce3fa2
Prestos <prestosapps@gmail.com>
2016-01-19 00:00:01 +0000

    Added fragment to check the IP address. Still a bit rough round the edges, needs tidying up and error handling

--- AdsbMonitor/app/src/main/java/com/prestos/adsbmonitor/AircraftActivity.java
+++ AdsbMonitor/app/src/main/java/com/prestos/adsbmonitor/AircraftActivity.java
@@ -11,14 +11,27 @@ public class AircraftActivity extends AppCompatActivity implements IpAddressDial
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aircraft);
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if (prefs.contains("prefs_ip_address")) {
-            Log.d(AircraftActivity.class.getName(), "found IP address preferences: " + prefs.getString("prefs_ip_address", null));
-            setContentView(R.layout.activity_aircraft);
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            if (prefs.contains("prefs_ip_address")) {
+                Log.d(AircraftActivity.class.getName(), "found IP address preferences: " + prefs.getString("prefs_ip_address", null));
+                AircraftActivityFragment aircraftActivityFragment = new AircraftActivityFragment();
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, aircraftActivityFragment).commit();
+            } else {
+                Log.d(AircraftActivity.class.getName(), "Can not find prefs_ip_address, so need to get it");
+                IpCheckFragment ipCheckFragment = new IpCheckFragment();
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, ipCheckFragment).commit();
+                createIpAddressDialog();
+            }
         } else {
-            Log.d(AircraftActivity.class.getName(), "Can not find prefs_ip_address, so need to get it");
-            createIpAddressDialog();
+            Log.d(AircraftActivity.class.getName(), "Can not find the necessary view");
         }
     }
 
@@ -30,5 +43,7 @@ public class AircraftActivity extends AppCompatActivity implements IpAddressDial
     @Override
     public void onDialogClick(String ipAddress) {
         Log.d(AircraftActivity.class.getName(), "Deal with " + ipAddress);
+        IpCheckFragment ipCheckFragment = (IpCheckFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        ipCheckFragment.checkIpAddress(ipAddress);
     }
 }
---
+++ AdsbMonitor/app/src/main/java/com/prestos/adsbmonitor/IpCheckFragment.java
@@ -0,0 +1,65 @@
+package com.prestos.adsbmonitor;
+
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.prestos.adsbmonitor.model.Receiver;
+
+import java.io.IOException;
+
+/**
+ * Created by prestos on 18/01/16.
+ */
+public class IpCheckFragment extends Fragment {
+
+    private TextView status;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ip_check, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        status = (TextView) getActivity().findViewById(R.id.fragment_ipcheck_status);
+    }
+
+    public void checkIpAddress(String ipAddress) {
+        Log.d(IpCheckFragment.class.getName(), "Fragment checking " + ipAddress);
+        status.setText("Checking " + ipAddress);
+        new ReceiverDataLoader().execute(ipAddress);
+    }
+
+    private class ReceiverDataLoader extends AsyncTask<String, Void, Receiver> {
+
+        private String URI = "/dump1090/data/receiver.json";
+
+        @Override
+        protected Receiver doInBackground(String... voids) {
+            String receiverUrl = "http://" + voids[0] + URI;
+            Receiver receiver = null;
+            try {
+                receiver = new Receiver(DataHandler.getData(receiverUrl));
+            } catch (IOException e) {
+                Log.e(IpCheckFragment.ReceiverDataLoader.class.getName(), "AAaarrggh!!", e);
+            }
+            return receiver;
+        }
+
+        @Override
+        protected void onPostExecute(Receiver receiver) {
+            super.onPostExecute(receiver);
+            status.setText("Found " + receiver.getVersion() + " version of Dump1090, good to go!");
+        }
+    }
+}
