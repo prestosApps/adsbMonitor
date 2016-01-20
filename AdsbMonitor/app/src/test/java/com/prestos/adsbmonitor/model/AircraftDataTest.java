456c28dc6dca88831ec3ebd34dcf2f68839e6be8
PrestosApps <prestosapps@gmail.com>
2016-01-20 22:43:27 +0000

    Start setting up unit tests

---
+++ AdsbMonitor/app/src/test/java/com/prestos/adsbmonitor/model/AircraftDataTest.java
@@ -0,0 +1,17 @@
+package com.prestos.adsbmonitor.model;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Created by prestos on 20/01/2016.
+ */
+public class AircraftDataTest {
+
+    @Test
+    public void aircraftData_object_constructed() {
+        Assert.fail("Not implemented");
+    }
+
+}
